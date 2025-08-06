c9577d1f371e8d5a5d57572c83c3ba12d008ce37
nana1313 <gibsonrichard419@gmail.com>
2025-08-06 07:12:04 -0700

    Added new text

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-console.log('Hello World');
-console.error('This is an error');
+// Arrays - variable that hold multiple values
+
+const fruits = ['apples', 'oranges', 'pears'];
+
+fruits[3] ='grapes';
+
+fruits.push('mango');
+
+fruits.unshift('strawberries');
+
+fruits.pop();
+
+console.log(Array.isArray('hello'));
+
+console.log(fruits.indexOf('oranges'));
+
+console.log(fruits);
