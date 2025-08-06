ac4e0572090f1eafcc255fd561ff1946485f07c9
nana1313 <gibsonrichard419@gmail.com>
2025-08-06 10:37:12 -0700

    Added new text

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-// Arrays - variable that hold multiple values
+const todos = [
+  {
+    id: 1,
+    text: 'take out trash',
+    isCompleted: true 
+  },
+  {
+    id: 2,
+    text: 'Meeting with boss',
+    isCompleted: true
+  },
+  {
+    id: 3,
+    text: 'Dentist appt',
+    isCompleted: false 
+  }
+];
 
-const fruits = ['apples', 'oranges', 'pears'];
+// forEach, map, filter
+const todoCompleted = todos.filter(function(todo) { 
+    return todo.isCompleted = true;
+}). map(function(todo) {
+    return todo.text;
+})    
 
-fruits[3] ='grapes';
+console.log(todoCompleted);
 
-fruits.push('mango');
 
-fruits.unshift('strawberries');
-
-fruits.pop();
-
-console.log(Array.isArray('hello'));
-
-console.log(fruits.indexOf('oranges'));
-
-console.log(fruits);
